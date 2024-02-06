915ffe1f546244dd0f49f125f02c6e80d75bef38
joaohenriquefsp <joaohenriquefsp@gmail.com>
2024-02-06 18:51:05 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiSearch} from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Teste Projeto</h1>
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu Cep.."
+        />
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+
+        <main className='main'>
+          <h2> CEP: 79003222</h2>
+          <span>
+            </span>>
+        </main>
+     
     </div>
   );
 }
